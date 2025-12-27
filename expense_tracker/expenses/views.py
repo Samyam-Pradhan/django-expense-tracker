4d617aacd62496fb26f774be84d5eb6be04d8b36
Samyam-Pradhan <pradhansamyam71@gmail.com>
2025-12-27 12:37:04 +0545

    Add expense list view, template, and URL routing

--- expense_tracker/expenses/views.py
+++ expense_tracker/expenses/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Expense
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def expense_list(request):
+    # Get expenses for the logged-in user
+    expenses = Expense.objects.filter(user=request.user).order_by('-date')
+    return render(request, 'expenses/expense_list.html', {'expenses': expenses})
