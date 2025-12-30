523e4d26081618aa13a05421ddb7a18b01fbe486
Samyam-Pradhan <pradhansamyam71@gmail.com>
2025-12-30 19:36:11 +0545

    Add create expense feature with form and view

--- expense_tracker/expenses/models.py
+++ expense_tracker/expenses/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.utils import timezone  # <- import this
 
 class Expense(models.Model):
     CATEGORY_CHOICES = [
@@ -15,7 +15,7 @@ class Expense(models.Model):
     title = models.CharField(max_length=100)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
-    date = models.DateField()
+    date = models.DateField(default=timezone.now)  # <- auto-fill today
     notes = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
